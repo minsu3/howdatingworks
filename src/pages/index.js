fe7180f0643fb0b00969e141434b958b4bde9de8
Min Su Kim <kim97minsu@gmail.com>
2021-01-20 21:22:18 -0800

    initialized commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,149 +1,132 @@
 import * as React from "react"
+import { Link } from "gatsby"
+import "../styles/home.css"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
+const container = {
+  minHeight: "100vh",
+  padding: "0",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
+const main = {
+  padding: "5rem 0",
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
+const header = {
+  color: "black",
+  fontSize: "30px",
+  width: "100%",
+  padding: "2rem",
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "space between"
 }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
+const homeTitle = {
+  color: "rgb(235, 146, 130)",
+  fontWeight: 100,
+  fontSize: "34px",
+  borderRadius: "5px",
+  textDecoration: "none",
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+const nav = {
+  textAlign: "center",
 }
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
+
+const title = {
+  margin: 0,
+  lineHeight: 1.15,
+  fontSize: "4rem",
+  width: "650px",
+  color: "rgb(235, 146, 130)",
+  fontFamily: "Noto Serif JP",
+  fontWeight: 100,
+}
+
+const contactContainer = {
+  float: "right",
+}
+
+const contact = {
+  padding: "10px 30px 10px 30px",
+  backgroundColor: "rgb(235, 146, 130)",
+  color: "white",
+  fontSize: "18px",
+}
+
+const footer = {
+  width: "100%",
+  height: "100px",
+  borderTop: "1px solid #eaeaea",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "white",
+}
+
+const logo = {
+  height: "1em",
+}
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <div style={container}>
+      <header style={header}>
+        <nav style={homeTitle}>
+          <Link to="/overview">
+            <p>How Dating Works</p>
+          </Link>
+        </nav>
+        <nav style={nav}>
+          <Link to="/about-me">
+            <p>About Me</p>
+          </Link>
+          <Link to="/core-principles">
+            <p>Core Principles</p>
+          </Link>
+          <Link to="/blog">
+            <p>Blog</p>
+          </Link>
+        </nav>
+        <nav style={contactContainer}>
+          <Link to="/exercise">
+            <p style={contact}>Contact</p>
+          </Link>
+        </nav>
+      </header>
+
+      <main style={main}>  
+        <h1 style={title}>
+          How to Meet, Date, and Keep the Women You've Always Wanted
+        </h1>
+
+        <a href="https://facebook.com/">
+          Book your Enlightenment Today
+        </a>
+      </main>
+
+      <footer style={footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" style={logo} />
+        </a>
+      </footer>
+    </div>
   )
 }
 
