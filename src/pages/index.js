ac90d62e212e1fadd6252568b9f812f5cd233859
Min Su Kim <kim97minsu@gmail.com>
2021-03-09 15:13:30 -0800

    inital layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,133 +1,128 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import "../styles/home.css"
+import * as React from "react";
+import { Link } from "gatsby";
+import "../styles/home.css";
+import "../images/mainLogo.png";
+import styles from "../styles/homepage.module.css";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const container = {
-  minHeight: "100vh",
-  padding: "0",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
+export const query = graphql`
+  query {
+    fileName: file(relativePath: { eq: "images/mainLogo.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+function HeaderFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
-const main = {
-  padding: "5rem 0",
-  flex: 1,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  position: "relative",
-}
-
-const header = {
-  color: "black",
-  fontSize: "30px",
-  width: "100%",
-  padding: "2rem",
-  alignItems: "center",
-  display: "flex",
-  flexDirection: "space between"
-}
-
-const homeTitle = {
-  color: "rgb(235, 146, 130)",
-  fontWeight: 100,
-  fontSize: "34px",
-  borderRadius: "5px",
-  textDecoration: "none",
-}
-
-const nav = {
-  textAlign: "center",
-}
-
-const title = {
-  margin: 0,
-  lineHeight: 1.15,
-  fontSize: "4rem",
-  width: "650px",
-  color: "rgb(235, 146, 130)",
-  fontFamily: "Noto Serif JP",
-  fontWeight: 100,
-}
-
-const contactContainer = {
-  float: "right",
-}
-
-const contact = {
-  padding: "10px 30px 10px 30px",
-  backgroundColor: "rgb(235, 146, 130)",
-  color: "white",
-  fontSize: "18px",
-}
-
-const footer = {
-  width: "100%",
-  height: "100px",
-  borderTop: "1px solid #eaeaea",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "white",
-}
-
-const logo = {
-  height: "1em",
-}
-
-// markup
 const IndexPage = () => {
   return (
-    <div style={container}>
-      <header style={header}>
-        <nav style={homeTitle}>
-          <Link to="/overview">
-            <p>How Dating Works</p>
-          </Link>
-        </nav>
-        <nav style={nav}>
-          <Link to="/about-me">
-            <p>About Me</p>
-          </Link>
-          <Link to="/core-principles">
-            <p>Core Principles</p>
-          </Link>
-          <Link to="/blog">
-            <p>Blog</p>
-          </Link>
-        </nav>
-        <nav style={contactContainer}>
-          <Link to="/exercise">
-            <p style={contact}>Contact</p>
-          </Link>
-        </nav>
-      </header>
+    <div>
+      <div>
+        <div className={styles.header}>
+          <div className={styles.topnav} id="myTopnav">
+            <a href="#" className={styles.mainLogo}>
+              <img src="mainLogo.png" />
+            </a>
+            <a href="#">News</a>
+            <a href="#">Contact</a>
+            <a href="#">About</a>
+            <a className={styles.icon} onClick={HeaderFunction}>
+              <FontAwesomeIcon icon={faBars} />
+            </a>
+          </div>
+        </div>
 
-      <main style={main}>  
-        <h1 style={title}>
-          How to Meet, Date, and Keep the Women You've Always Wanted
-        </h1>
+        <div className={styles.main}>
+          <h2 style={{ fontSize: "50px", color: "white" }}>How Dating Works</h2>
+        </div>
 
-        <a href="https://facebook.com/">
-          Book your Enlightenment Today
-        </a>
-      </main>
+        <div className={styles.content}>
+          <div className={styles.leftcolumn}>
+            <div className={styles.card}>
+              <h2>TITLE HEADING</h2>
+              <h5>Title description, Dec 7, 2017</h5>
+              <div className={styles.fakeimg}>Image</div>
+              <p>Some text..</p>
+              <p>
+                Sunt in culpa qui officia deserunt mollit anim id est laborum
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco.
+              </p>
+            </div>
 
-      <footer style={footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" style={logo} />
-        </a>
-      </footer>
+            <div className={styles.card}>
+              <h2>TITLE HEADING</h2>
+              <h5>Title description, Sep 2, 2017</h5>
+              <div className={styles.fakeimg}>Image</div>
+              <p>Some text..</p>
+              <p>
+                Sunt in culpa qui officia deserunt mollit anim id est laborum
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco.
+              </p>
+            </div>
+
+            <div className={styles.card}>
+              <h2>TITLE HEADING</h2>
+              <h5>Title description, Sep 2, 2017</h5>
+              <div className={styles.fakeimg}>Image</div>
+              <p>Some text..</p>
+              <p>
+                Sunt in culpa qui officia deserunt mollit anim id est laborum
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.rightcolumn}>
+            <div className={styles.card}>
+              <h2>About Me</h2>
+              <div className={styles.fakeimg} style={{ height: "100px" }}>
+                Image
+              </div>
+              <p>
+                Some text about me in culpa qui officia deserunt mollit anim..
+              </p>
+            </div>
+
+            <div className={styles.card}>
+              <h3>Popular Post</h3>
+              <div className={styles.fakeimg}>Image</div>
+              <br />
+              <div className={styles.fakeimg}>Image</div>
+              <br />
+              <div className={styles.fakeimg}>Image</div>
+            </div>
+
+            <div className={styles.card}>
+              <h3>Follow Me</h3>
+              <p>Some text..</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div>Footer</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
